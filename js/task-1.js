8e1804da6f85bfd2ef30f01c830501c00716b270
AgentKisa <155394878+AgentKisa@users.noreply.github.com>
2024-05-04 23:16:03 +0300

    task-1

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelector('#categories');
-
-console.log('Numbers of categories:', list.children.length);
-
-Array.from(list.children).forEach(item => {
-  console.log('Category:', item.querySelector('h2').textContent);
-  console.log('Elements:', item.querySelectorAll('li').length);
-});
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const text = document.querySelector('#name-input');
-const pismo = document.querySelector('#name-output');
-
-text.addEventListener(
-  'input',
-  event => (pismo.textContent = event.currentTarget.value.trim() || 'Anonymous')
-);
